4c3eecd84da4050b380d376650d49afd67c938a5
JianXing <simple_star@126.com>
2020-06-04 08:57:33 +0800

    add gini impurity and entropy

--- intelligence/decision tree/tree_predict.py
+++ intelligence/decision tree/tree_predict.py
@@ -1,5 +1,10 @@
+from collections import Counter
+from functools import reduce
+import operator
+import math
+
 # 结点数据结构
-class decision_node:
+class decisionNode:
     def __init__(slef, col=-1, value=None, results=None, tb=None, fb=None):
         self.col = col # 属性列值
         self.value = value # 判定条件值（一般是判定为True的值）
@@ -10,7 +15,7 @@ class decision_node:
 # 数据集划分函数
 # 根据某一列的值将数据划分成两个集合
 # 处理数值数据和字符串数据
-def divide_set(rows, column, value):
+def divideSet(rows, column, value):
     # 定义一lambda函数，用于划分判断 
     split_function = None
     if isinstance(value, int) or isinstance(value, float):
@@ -25,29 +30,22 @@ def divide_set(rows, column, value):
     return (set1, set2)
 
 
-#结果值计数
-#从分类结果中找出每种结果的个数
-def unique_counts(rows):
-    results = {} # 用字典来保存各种结果的计数
-    for row in rows:
-        r = row[len(row)-1]
-        if r not in results:
-            results[r] = 0 # 初始化字典项，key=r  value=0
-        results[r]+=1 # 按key值r来索引value并+1
-    return results
+# 计算基尼不纯度
+# input: 数据集列表(其中最后一列为标签)
+# output: 这一分类数据集的基尼不纯度(float值)
+def calcuGini(dataSet):
+    labelCounts = Counter(entry[-1] for entry in dataSet) # 生成标签计数字典(取最后一列为标签值)
+    probability = [float(v)/sum(labelCounts.values()) for v in labelCounts.values()] # 计算各标签概率
+    return 1 - reduce(operator.add, map(lambda x: x**2, probability )) # 返回 gini impurity
     
-#基尼不纯度
-def gini_impurity(rows):
-    total = len(rows)
-    lab_counts = unique_counts(rows)
-    imp = 0
-    for k1 in lab_counts:
-        p1 = float(lab_counts[k1])/total
-        for k2 in lab_counts:
-            if k1 == k2: continue
-            p2 = float(lab_counts[k2])/total
-            imp+=p1*p2
-    return imp
+    
+#计算数据集的信息熵
+#input: 给定数据集
+#output: 该数据集的信息熵
+def calcuEntrop(dataSet):
+    labelCounts = Counter(entry[-1] for entry in dataSet)
+    probability = [float(v)/sum(labelCounts.values()) for v in labelCounts.values()]
+    return -1*reduce(operator.add, map(lambda x:x*math.log(x,2), probability ))
 
 
 if __name__ == '__main__':
@@ -58,19 +56,22 @@ if __name__ == '__main__':
     data = [[item[0], item[1], item[2], int(item[3]), item[4]] for item in raw]
     
     # 按某一列值分割数据集
-    (set1, set2) = divide_set(data, 2, 'No')
-    print([item[4] for item in set1])
-    print([item[4] for item in set2])
+    (set1, set2) = divideSet(data, 4, 'None')
+    print([item[-1] for item in set1])
+    print([item[-1] for item in set2])
     
-    # 统计这一分割下标签的划分情况
-    set3 = unique_counts(set1)
-    set4 = unique_counts(set2)
-    print(set3)
-    print(set4)
+    # 计算 gini impurity
+    g3 = calcuGini(set1)
+    g4 = calcuGini(set2)
+    print("Gini impurity:")
+    print(g3)
+    print(g4)
     
-    # 计算每个分类的基尼不纯度
-    g1 = gini_impurity(set3)
-    g2 = gini_impurity(set4)
-    print(g1)
-    print(g2)
+    # 计算 信息熵
+    entropy1 = calcuEntrop(set1)
+    entropy2 = calcuEntrop(set2)
+    print("Entropy:")
+    print(entropy1)
+    print(entropy2)
+    
     
